9c6d0cdf01f7796c55aac7662a3d53e9dc549b9f
Ram Kinkar Singh <ramkinkar2512@gmail.com>
2022-07-17 19:27:05 +0530

    adding screens

---
+++ lib/screens/library.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class YourLibrary extends StatelessWidget {
+  const YourLibrary({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'YourLibrary',
+      style: TextStyle(
+        color: Colors.green,
+        fontSize: 40,
+      ),
+    );
+  }
+}
---
+++ lib/screens/search.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Search',
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 40,
+      ),
+    );
+  }
+}
