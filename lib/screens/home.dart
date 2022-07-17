d9083fc3b718d175120776bb9fc1019ec74c5658
Ram Kinkar Singh <ramkinkar2512@gmail.com>
2022-07-17 21:10:44 +0530

    adding AppBar to home screen
    using Column Widget as a child of container . First child of column widget is AppBar that contains tittle and setting action icon

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,10 +3,27 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget createAppBar(String message) {
+    return AppBar(
+      title: Text(message),
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: Icon(Icons.settings),
+        ),
+      ],
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
+        child: Column(
+          children: [createAppBar('Welcome Back')],
+        ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [Colors.green, Colors.black],
